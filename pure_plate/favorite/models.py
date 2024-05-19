9238425b33148fe61c92c0f656ec3a59a44fc60b
saeyeonn <sy01lim@naver.com>
2024-05-19 20:18:10 +0900

    Feat: Add / Delete / View favorites of user

--- pure_plate/favorite/models.py
+++ pure_plate/favorite/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from account.models import User
+from restaurant.models import Restaurant
 
-# Create your models here.
+class Favorite(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('user', 'restaurant')
