5ba2f4d8f4dac4329dfc574a95b101bacd7879df
ChanHHaeng <dlcksgod2325@gmail.com>
2024-05-26 22:43:50 +0900

    refactor:change category from list to boolean

--- pure_plate/restaurant/views.py
+++ pure_plate/restaurant/views.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
 from django.db.models import Count
-from .models import Restaurant, Category
+from .models import Restaurant
 
 #/api/restaurants_in_categories?categories=Italian,Chinese,Mexican
 
@@ -17,27 +17,36 @@ def restaurants_in_categories_view(request):
 
         category_names_list = category_names.split(',')
 
-        # find all restaurants in selected categories
-        categories = Category.objects.filter(CategoryName__in=category_names_list)
-        restaurants = Restaurant.objects.filter(categories__in=categories).distinct()
+        filter_conditions = {}
+        if 'vegan' in category_names_list:
+            filter_conditions['vegan'] = True
+        if 'halal' in category_names_list:
+            filter_conditions['halal'] = True
+        if 'glutenfree' in category_names_list:
+            filter_conditions['gluten_free'] = True
+        if 'lactofree' in category_names_list:
+            filter_conditions['lacto_free'] = True
 
-        # filtered restaurants list category by category
-        data = {}
-        for category_name in category_names_list:
-            category_restaurants = restaurants.filter(categories__CategoryName=category_name)
-            category_list = [{
-                'restaurantId': restaurant.id,
-                'restaurantName': restaurant.name,
-                'restaurantAddress': restaurant.address,
-                'restaurantLatitude': float(restaurant.latitude),
-                'restaurantLongitude': float(restaurant.longitude),
-                'restaurantTime': restaurant.time,
-                'restaurantPhoto': restaurant.photo,
-                'restaurantPhone': restaurant.phone,
-                'restaurantReviewCount': restaurant.review_count,
-                'restaurantRating': str(restaurant.avg_rating),
-            } for restaurant in category_restaurants]
-            data[f"{category_name}List"] = category_list
+        restaurants = Restaurant.objects.filter(**filter_conditions)
+
+
+        data = [{
+            'restaurantId': restaurant.id,
+            'restaurantName': restaurant.name,
+            'restaurantAddress': restaurant.address,
+            'restaurantLatitude': str(restaurant.latitude),
+            'restaurantLongitude': str(restaurant.longitude),
+            'restaurantTime': restaurant.time,
+            'restaurantPhoto': restaurant.photo,
+            'restaurantPhone': restaurant.phone,
+            'restaurantReviewCount': restaurant.review_count,
+            'restaurantRating': str(restaurant.avg_rating),
+            'restaurantVegan': restaurant.vegan,
+            'restaurantHalal': restaurant.halal,
+            'restaurantGlutenFree': restaurant.gluten_free,
+            'restaurantLactoFree': restaurant.lacto_free
+        } for restaurant in restaurants]
+
 
         response = {
             'status': 200,
